b81da077bcef8f08a4bfa2b83202b7b134fa1dba
jatin sidana <jatinsadana1998@gmail.com>
2023-07-09 13:35:04 +0530

    sound added to one button

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-document.querySelector("button").addEventListener('click',handleCliked) //only for first button
+ document.querySelector("button").addEventListener('click',handleCliked) //only for first button
 
-function handleCliked(){
-    alert("i got clicked");
-}
+ function handleCliked(){
+  alert("i got clicked");
+  }
    // for all the buttons
+
+
+ //  var allnumber = document.querySelectorAll(".drum").length
+  // for(var i =0; i<allnumber;i++){
+   // document.querySelectorAll("button")[i].addEventListener('click',handleCliked);
+
+  // }
+
+  // playing sounds
+
+  
   var allnumber = document.querySelectorAll(".drum").length
    for(var i =0; i<allnumber;i++){
-    document.querySelectorAll("button")[i].addEventListener('click',handleCliked);
+    document.querySelectorAll(".drum")[i].addEventListener('click',function(){
+        var audio = new Audio("sounds/tom-1.mp3")
+        audio.play();
+    });
 
    }
