e088e97c9b8ccfa026c8932b6a753a00564b32fb
jatin sidana <jatinsadana1998@gmail.com>
2023-07-09 14:18:38 +0530

    sounds added to all btn

--- index.js
+++ index.js
@@ -1,25 +1,64 @@
+ 
+ {/* 
+ 
  document.querySelector("button").addEventListener('click',handleCliked) //only for first button
 
  function handleCliked(){
   alert("i got clicked");
   }
-   // for all the buttons
+    for all the buttons
 
 
- //  var allnumber = document.querySelectorAll(".drum").length
-  // for(var i =0; i<allnumber;i++){
-   // document.querySelectorAll("button")[i].addEventListener('click',handleCliked);
+   var allnumber = document.querySelectorAll(".drum").length
+   for(var i =0; i<allnumber;i++){
+    document.querySelectorAll("button")[i].addEventListener('click',handleCliked);
 
-  // }
+   }
+ 
 
+  */}
   // playing sounds
 
   
   var allnumber = document.querySelectorAll(".drum").length
    for(var i =0; i<allnumber;i++){
     document.querySelectorAll(".drum")[i].addEventListener('click',function(){
-        var audio = new Audio("sounds/tom-1.mp3")
-        audio.play();
+        var buttonInnerHtml = this.innerHTML;
+        
+        switch (buttonInnerHtml) {
+            case "w":
+                var crash = new Audio("sounds/crash.mp3")
+                 crash.play();
+                break;
+                case "a":
+                var kick = new Audio("sounds/kick-bass.mp3")
+                 kick.play();
+                break;
+                case "s":
+                    var snare = new Audio("sounds/snare.mp3")
+                     snare.play();
+                    break;
+                    case "d":
+                var tom1 = new Audio("sounds/tom-1.mp3")
+                 tom1.play();
+                break;
+                case "j":
+                var tom2 = new Audio("sounds/tom-2.mp3")
+                 tom2.play();
+                break;
+                case "k":
+                var tom3 = new Audio("sounds/tom-3.mp3")
+                 tom3.play();
+                break;
+                case "l":
+                var tom4 = new Audio("sounds/tom-4.mp3")
+                 tom4.play();
+                break;
+            default:console.log(buttonInnerHtml)
+            
+        }
+       
     });
 
    }
+
