a7ece47bd153986fbf6bb7b96d11b5c39af40c16
jatin sidana <jatinsadana1998@gmail.com>
2023-07-10 11:41:58 +0530

    keypress from keybord will make sound

--- index.js
+++ index.js
@@ -24,45 +24,46 @@
    for(var i =0; i<allnumber;i++){
     document.querySelectorAll(".drum")[i].addEventListener('click',function(){
         var buttonInnerHtml = this.innerHTML;
-        
-        switch (buttonInnerHtml) {
-            case "w":
-                var crash = new Audio("sounds/crash.mp3")
-                 crash.play();
-                break;
-                case "a":
-                var kick = new Audio("sounds/kick-bass.mp3")
-                 kick.play();
-                break;
-                case "s":
-                    var snare = new Audio("sounds/snare.mp3")
-                     snare.play();
-                    break;
-                    case "d":
-                var tom1 = new Audio("sounds/tom-1.mp3")
-                 tom1.play();
-                break;
-                case "j":
-                var tom2 = new Audio("sounds/tom-2.mp3")
-                 tom2.play();
-                break;
-                case "k":
-                var tom3 = new Audio("sounds/tom-3.mp3")
-                 tom3.play();
-                break;
-                case "l":
-                var tom4 = new Audio("sounds/tom-4.mp3")
-                 tom4.play();
-                break;
-            default:console.log(buttonInnerHtml)
-            
-        }
+        makeSound(buttonInnerHtml);
        
     });
 
    }
-
-   document.addEventListener("keypress", function(){
-    alert("i got clicked");
+       /*for keybord press key */
+   document.addEventListener("keypress", function(event){
+   makeSound(event.key);
    })
+   function makeSound(key){
+    switch (key) {
+        case "w":
+            var crash = new Audio("sounds/crash.mp3")
+             crash.play();
+            break;
+            case "a":
+            var kick = new Audio("sounds/kick-bass.mp3")
+             kick.play();
+            break;
+            case "s":
+                var snare = new Audio("sounds/snare.mp3")
+                 snare.play();
+                break;
+                case "d":
+            var tom1 = new Audio("sounds/tom-1.mp3")
+             tom1.play();
+            break;
+            case "j":
+            var tom2 = new Audio("sounds/tom-2.mp3")
+             tom2.play();
+            break;
+            case "k":
+            var tom3 = new Audio("sounds/tom-3.mp3")
+             tom3.play();
+            break;
+            case "l":
+            var tom4 = new Audio("sounds/tom-4.mp3")
+             tom4.play();
+            break;
+        default:console.log(buttonInnerHtml)
+    }
+   }
 
